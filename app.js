ca009b0b7edf757efbaeeb0c8d3774a4918a95ec
Ali Shah <s.ashah@outlook.com>
2021-01-22 03:01:38 -0500

    [Added] templating middleware (handlebars)

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs = require('express-handlebars');
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-  res.send("Salaam, Squirrel! :)");
+  // set the GIF's URL
+  const gifUrl = 'https://media1.tenor.com/images/64c42357f26497ef96f38b9fc530b078/tenor.gif?itemid=17594271';
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl });
+});
+
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
 });
 
 // Start Server
