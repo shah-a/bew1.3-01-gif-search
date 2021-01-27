803efca543ecfdd93082407f18b60ec4f75b7c2c
Ali Shah <s.ashah@outlook.com>
2021-01-27 05:33:20 -0500

    [Added] working API call to Tenor
    Other changes:
    - [Added] dotenv to hide API key

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // Require Libraries
 const express = require('express');
+// const https = require('https');
+
+require('dotenv').config();
+
+const Tenor = require('tenorjs').client({
+  'Key': process.env.API_KEY,
+  'Filter': 'high',
+  'Locale': 'en_US'
+});
 
 // App Setup
 const app = express();
@@ -7,14 +16,31 @@ const app = express();
 // Middleware
 const exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-  console.log(req.query)
-  res.render('home');
+  // Handle the home page when we haven't queried yet
+  term = ""
+  if (req.query.term) {
+    term = req.query.term;
+  }
+
+  // Ternary method:
+  // term = (req.query.term ? req.query.term : "");
+
+  Tenor.Search.Query(term, "10")
+    .then(response => {
+      const gifs = response;
+      res.render('home', { gifs });
+    }).catch(console.error);
 });
+
+// app.get('/stretch', (req, res) => {
+//   // Stretch Challenge:
+//   // Perform the API call with 'https' module instead of 'tenorjs'
+// });
 
 // Start Server
 app.listen(3000, () => {
